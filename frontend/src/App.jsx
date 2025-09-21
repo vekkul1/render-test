fbf1db908bf061afa950f91ff1a425d5f151af95
vekkul1 <veeti.hallaaho@gmail.com>
2025-09-21 03:14:25 +0300

    pls work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -46,7 +46,6 @@ const App = () => {
 
   const toggleImportanceOf = (id) => {
     console.log("imp of ", id)
-    const url = `http://localhost:3001/api/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = {...note, important: !note.important }
 
